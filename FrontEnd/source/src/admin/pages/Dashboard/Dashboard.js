93bf681a933880191547aef2c337b04a26d54790
phantanloc1988 <phantanloc1988@gmail.com>
2024-11-28 20:13:07 +0700

    test pr

--- FrontEnd/source/src/admin/pages/Dashboard/Dashboard.js
+++ FrontEnd/source/src/admin/pages/Dashboard/Dashboard.js
@@ -4,7 +4,7 @@ import BreadCrumb from "../../common/components/TopTitle";
 const Dashboard = () => {
   return (
     <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
-       huhu
+       huhu 11
     <BreadCrumb title='Trang chủ'/>
   </div>
   );
